cc6cfa71e30de9acc5144349fde8ac93bbd9ce68
jin1125 <shogo.jinta@gmail.com>
2021-10-13 21:46:41 +0900

    first commit

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+it('Should render hello text', () => {
+  render(<Home />)
+  // screen.debug()
+  expect(screen.getByText('Hello Nextjs')).toBeInTheDocument()
+})
